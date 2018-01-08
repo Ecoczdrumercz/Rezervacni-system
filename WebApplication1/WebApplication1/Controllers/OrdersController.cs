b51340e9de3cd8afc9cb8e3f46c70391d705f01c
DanielP <DanielP@DESKTOP-46DDJ4V>
2018-01-08 21:00:00 +0100

    test

--- WebApplication1/WebApplication1/Controllers/OrdersController.cs
+++ WebApplication1/WebApplication1/Controllers/OrdersController.cs
@@ -46,7 +46,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,DateCreated")] Order order)
+        public ActionResult Create([Bind(Include = "Id,DateCreated,Reservation,Order")] Order order)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,DateCreated")] Order order)
+        public ActionResult Edit([Bind(Include = "Id,DateCreated,Order")] Order order)
         {
             if (ModelState.IsValid)
             {
--- WebApplication1/WebApplication1/Controllers/ReservationsController.cs
+++ WebApplication1/WebApplication1/Controllers/ReservationsController.cs
@@ -46,7 +46,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Hour")] Reservation reservation)
+        public ActionResult Create([Bind(Include = "Id,Hour,Place")] Reservation reservation)
         {
             if (ModelState.IsValid)
             {
